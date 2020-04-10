b1b0c1c4f305f7de97eb037b8e03c58bf28c44fa
user6174 <enne100724@gmail.com>
2020-04-10 19:37:42 +0200

    ~

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
-import discord
-from images import *
+import discord  # https://discordpy.readthedocs.io/en/latest/api.html
+# from images import *
 from cards import *
 
 token = 'Njg0MTQyODIwMTIyMjk2MzQ5.Xl13Ww.52HjCT9BrAxD75rf-TPHKe6dD2s'
 
 p = Pool()
+
+
+def cardToEmbed(card):
+    embed = discord.Embed(title="{} {} {}".format(card.rarity, card.craft, card.type))
+    embed.set_image(url=card.pic)
+    embed.add_field(name="effect:", value=card.effect)
+    embed.set_footer(text=card.flair)
+    return embed
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged in as')
@@ -16,7 +26,10 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
         if message.content.startswith('+random'):
-            await message.channel.send(p.randomCard().name)
+            randomCard = cardToEmbed(p.getRandomCard())
+            await message.channel.send(embed=randomCard)
+            message.react(":regional_indicator_e:")
+
 
 client = MyClient()
 client.run(token)
--- cards.py
+++ cards.py
@@ -4,12 +4,15 @@ import random
 with open("en.json", 'r') as f:
     data = json.load(f)
 
+
 class Card:
+
     def __init__(self, name):
         card = data[name]
         self.name = card['name']
         self.id = card['id']
         self.pic = "https://sv.bagoum.com/cardF/en/c/{}".format(self.id)
+        self.evopic = "https://sv.bagoum.com/cardE/en/c/{}".format(self.id)
         self.craft = card['faction']
         self.rarity = card['rarity']
         self.trait = card['race']
@@ -17,30 +20,43 @@ class Card:
         self.type = card['type']
         self.pp = card['manaCost']
         self.flair = card['baseData']['flair']
-        self.effect = card['baseData']['description']
+        self.effect = card['baseData']['description'].replace('<br>', '')
         self.attack = card['baseData']['attack']
         self.defense = card['baseData']['defense']
-        
+
     def __str__(self):
-        print(self.name)
-        print(self.pic)
-        ###more
-        return '\n'
+        return "\n{}" \
+               "\n{}" \
+               "\n {}/{} {} {} {}".format(self.pic,
+                                          self.name,
+                                          self.attack, self.defense, self.rarity, self.craft, self.type)
+
 
 class Pool:
+
     def __init__(self, expac=''):
-        self.cards = [Card(data[i]['name']) for i in data if data[i]['expansion']!= 'Token']
-        if expac!='':
+        self.cards = [Card(data[i]['name']) for i in data if data[i]['expansion'] != 'Token']
+        if expac != '':
             self.cards = [i for i in self.cards if expac == i.expac]
-    def randomCard(self):
+
+    def __getitem__(self, index):
+        return self.cards[index]
+
+    def getRandomCard(self):
         return self.cards[random.randint(0, len(self.cards))]
+
 
 ### ONLY RUN THIS IF YOU DON'T HAVE THE IMAGE ASSETS
 ### RUN THIS FUNCTION IN THE DIRECTORY YOU WISH THE IMAGES TO BE SAVED IN
 ### THE CARD IMAGES AMOUNT TO ~1.5GB
 ### if the download gets stuck on a particular use a skip flag
-def download_pics():    
+def download_pics():
     import urllib.request
     for i in cards:
         print("downloading {}".format(i.name))
         urllib.request.urlretrieve(i.pic, "{}.jpg".format(i.name))
+
+
+def moduleTest():
+    p = Pool()
+    print(p[0])
