0abe592d129d0cfcc72a9a58b162278e69401a88
user6174 <enne100724@gmail.com>
2020-04-13 18:00:22 +0200

    ~

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from table_bot import *
 pool = Pool()
 token = 'Njg0MTQyODIwMTIyMjk2MzQ5.Xl13Ww.52HjCT9BrAxD75rf-TPHKe6dD2s'
 BOT_PREFIX = '+'
+MAX_MATCHES = 15
 
 
 class MyClient(discord.Client):
@@ -15,42 +16,54 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         if message.author.id == self.user.id:
-            if "Follower" in message.embeds[0].description \
-                    and "[Evolved]" not in message.embeds[0].title:
-                await message.add_reaction("🇪")  # regional indicator E
-            return  # to avoid unaccounted for self-replies
+            reactions = []
+            if react_to_card_embed(message.embeds):
+                reactions = reactions_to_card_embed()
+            elif react_to_search_list(message.embeds):
+                reactions = reactions_to_search_list(message.embeds)
+            if reactions:
+                for i in reactions:
+                    await message.add_reaction(i)
+            return
 
         if message.content.startswith(BOT_PREFIX):
             messageString = message.content.replace(BOT_PREFIX, '')
+            # out = ""
             dm = ""  # a possible direct message to be sent
 
             # Draft commands
             if messageString.startswith("join"):
-                out, dm = out_join(message.author)
+                out, dm = send_join(message.author)
             elif messageString.startswith("players"):
-                out = out_player_list()
+                out = send_player_list()
             elif messageString.startswith("leave"):
-                out, dm = out_leave(message.author)
+                out, dm = send_leave(message.author)
 
             # Card display commands
             elif messageString.startswith("random"):
-                out = out_random_card()
+                out = send_random_card()
             elif messageString[0] == messageString[-1] == "\"":
-                out = out_exact_search(messageString)
+                out = send_exact_search(messageString)
             else:
-                out = out_search(messageString)
+                out = send_search(messageString, MAX_MATCHES)
 
             await message.channel.send(embed=out)
             if dm != "":
                 await message.author.send(dm)
 
     async def on_reaction_add(self, reaction, user):
-        if reaction.message.author.id == self.user.id \
-                and user != self.user \
-                and reaction.message.embeds:
-            if "Follower" in reaction.message.embeds[0].description:
-                cardName = reaction.message.embeds[0].title
-                await reaction.message.channel.send(embed=card_to_embed(cardName, evo=True))
+        out = ""
+        dm = ""
+        if reaction.message.author.id == self.user.id and user != self.user:
+            if requested_send_card_evo(reaction):
+                out = send_card(card_name_from_embeds(reaction.message.embeds), evo=True)
+            if requested_send_card_from_list(reaction):
+                out = send_card_from_list(reaction)
+
+            if out != "":
+                await reaction.message.channel.send(embed=out)
+            if dm != "":
+                await reaction.message.author.send(dm)
 
 
 client = MyClient()
