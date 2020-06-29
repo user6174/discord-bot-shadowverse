6ede445f91b8a7540993ba11c01e505d379ce065
user6174 <enne100724@gmail.com>
2020-06-30 00:27:26 +0200

    updated to new json formatting

--- Pool.py
+++ Pool.py
@@ -1,69 +1,38 @@
-from Card import *
 import random
 import os
 import json
 from fuzzywuzzy import fuzz
 
+MAX_MATCHES = 15
+
+
+class TooManyMatches(Exception):
+    def __init__(self, matches):
+        self.matches = matches
+
 
 class Pool:
-    def __init__(self, expacs=[''], token=True):
-        try:
-            sets = ["Basic", "Standard Card Pack", "Darkness Evolved", "Rage of Bahamut", "Tempest of the Gods",
-                    "Wonderland Dreams", "Starforged Legends", "Chronogenesis", "Dawnbreak Nightedge",
-                    "Brigade of the Sky", "Omen of the Ten", "Altersphere", "Steel Rebellion", "Rebirth of Glory",
-                    "Verdant Conflict", "Ultimate Colosseum", "World Uprooted"]
-            assert [i in sets + [''] for i in expacs]
-        except AssertionError:
-            print("Invalid expansion name!")
+    def __init__(self):
+        with open(f'{os.getcwd()}/shadowverse-json/all.json', 'r') as f:
+            self.p = json.load(f)
 
-        self.cards = {}  # Key: card name - Value: the relative Card object.
-        self.name_to_index = {}  # One-to-one maps card names to integers.
-        with open("en.json", 'r') as f:
-            data = json.load(f)
-            count = 0
-            for i in data:
-                if data[i]["expansion"] == 'Token' and not token \
-                        or data[i]["expansion"] not in expacs and expacs != ['']:
-                    pass
-                else:
-                    self.cards[data[i]["name"]] = Card(data[i])
-                    self.name_to_index[data[i]["name"]] = count
-                    count += 1
-        self.index_to_name = {i: j for j, i in self.name_to_index.items()}  # Reverses nameToIndex's mapping.
+    def searchable(self, name):
+        c = self.p[name]
+        return f'{c["pp_"]}pp {c["rarity_"]} {c["craft_"]} {c["trait_"]} {c["type_"]} {c["expansion_"]} ' \
+               f'{c["baseAtk_"]}/{c["baseDef_"]} {"Rotation" if c["rotation_"] else "Unlimited"} ' \
+               f'{c["baseEffect_"]} {c["evoEffect_"]}'.lower()
 
-    """
-    data is a list of dictionaries (cards), whose key is the card name. Example:
-    >>> data["Robogoblin"].keys()
-        dict_keys(['name', '_name', 'id', 'faction', '_faction', 'rarity', '_rarity', 'race', 'expansion', '_expansion',
-        'type', '_type', 'hasEvo', 'hasAlt', 'hasAlt2', 'manaCost', 'baseData', 'evoData', 'rot', 'searchableText'])
-    Note that baseData and evoData are dictionaries themselves:
-    >>> data["Robogoblin"]["evoData"].keys()
-        dict_keys(['description', 'flair', 'attack', 'defense'])
-    """
-
-    def __getitem__(self, key):
-        return self.cards[key]
-
-    def __iter__(self):
-        return enumerate(self.cards)
-
-    def __contains__(self, item):
-        return item in self.cards
-
-    def get_card_name(self, index):
-        return self.index_to_name[index]
-
-    def get_card_index(self, name):
-        return self.name_to_index[name]
+    def pic(self, name, evo: bool):
+        return f'https://sv.bagoum.com/cardF/en/{"e" if evo else "c"}/{self.p[name]["id_"]}'
 
     def get_random_card(self):
-        return self.cards[random.choice(list(self.cards.keys()))].name
+        return self.p[random.choice(list(self.p.keys()))]["name_"]
 
-    def search_by_name(self, text, max_matches=99, similarity_threshold=70):
+    def search_by_name(self, text, max_matches=MAX_MATCHES, similarity_threshold=70):
         text = text.lower()
         result = []
         fuzzy_result = []
-        for i in self.cards:
+        for i in self.p:
             similarity = fuzz.partial_ratio(text, i.lower())
             if text in i.lower():
                 result.append(i)
@@ -72,68 +41,32 @@ class Pool:
         if not result:
             return fuzzy_result
         if len(result) > max_matches:
-            return len(result)
+            raise TooManyMatches(len(result))
         return result
 
-    def search_by_attributes(self, *attributes: list, max_matches=99):
+    def search_by_attributes(self, attributes: str, max_matches=MAX_MATCHES):
         result = []
-        for i in self.cards:
-            if False not in [j in self.cards[i].searchable for j in attributes]:
-                result.append(i)
+        for card in self.p:
+            if all(attr in self.searchable(card) for attr in attributes.lower().split(' ')):
+                result.append(card)
         if len(result) > max_matches:
-            return len(result)
+            raise TooManyMatches(len(result))
         return result
-
-
-def assets_downloader():
-    from urllib.request import urlretrieve
-    p = Pool()
-    for i in p.cards:
-        if not os.path.exists("assets/{}.jpg".format(p[i].id)):
-            urlretrieve(p[i].pic, "assets/{}.jpg".format(p[i].id))
-            print(i)
-
-
-def grid_merge(*images, scaling=200, row_length=4):
-    from PIL import Image
-    from time import time
-    pool = Pool()
-    images = [Image.open("assets/" + pool[i].id + ".jpg") for i in images]
-    images = [i.resize((scaling, int(i.size[1] / i.size[0] * scaling))) for i in images]
-    max_w = max([i.size[0] for i in images])
-    max_h = max([i.size[1] for i in images])
-    result_w = min(len(images), row_length) * max_w
-    result_h = (len(images) // row_length + 1) * max_h
-    result = Image.new('RGBA', (result_w, result_h))
-    for i in range(len(images)):
-        result.paste(im=images[i], box=((i % row_length) * max_w, (i // row_length) * max_h))
-    filename = str(int(time()))
-    result.save(filename + '.png')
-
-    return filename + '.png'
 
 
 def module_test():
-    def print_function_test(text, f, *args):
-        print('\n' + text)
-        print("inputs: {}\noutput: {}".format(list(args), f(*args)))
-
     p = Pool()
-    print("Testing Card.py:\n", p["Robogoblin"])
-    print_function_test("Random card:", p.get_random_card)
-    print_function_test("Name search, single result:", p.search_by_name, "roly", 2)
-    print_function_test("Name search, single result, with typo:", p.search_by_name, "rolyy", 2)
-    print_function_test("Name search, multiple results:", p.search_by_name, "tia", 99)
-    print_function_test("Name search, multiple results, with typo:", p.search_by_name, "gablin", 99)
-    print_function_test("Name search, results over threshold:", p.search_by_name, "a", 4)
-    print_function_test("Search by attributes, single result:", p.search_by_attributes, "2/2", "blood", "gold", "bat")
-    print_function_test("Search by attributes, multiple results:", p.search_by_attributes, "7/4")
-    print("\n{}'s index: {}".format("Robogoblin", p.get_card_index("Robogoblin")))
-    print("\nName of card #{}: {}".format(167, p.get_card_name(167)))
-    print("\nLoading the database up to DE and printing the first 10 cards by index:")
-    p = Pool(expacs=["Basic", "Standard", "Darkness Evolved"], token=False)
-    for i in range(10):
-        print("{} (from {})".format(p.get_card_name(i), p.cards[p.get_card_name(i)].expac))
+    print(p.searchable("Robogoblin"))
+    print(p.get_random_card())
+    print(p.search_by_name("Vania"))
+    print(p.search_by_name("Abominecion"))
+    try:
+        print(p.search_by_attributes("2/2"))
+    except TooManyMatches as t:
+        print(f'found {t.matches} matches')
+    print(p.search_by_attributes("2/2 gold neutral banish"))
+    print(p.pic("Goblin", True))
+    print(p.search_by_attributes("7/4"))
+    print(p.searchable("Hulking Dragonewt"))
 
-
-# inmodule_test()
+# module_test()
--- bot.py
+++ bot.py
@@ -1,13 +1,9 @@
 import asyncio
 import discord  # https://discordpy.readthedocs.io/en/latest/api.html
 from discord.ext import commands  # https://discordpy.readthedocs.io/en/latest/ext/commands/commands.html
-
-from Pool import *
-
-pool = Pool()
-
 import logging
 import sys
+from Pool import *
 
 root = logging.getLogger()
 root.setLevel(logging.INFO)
@@ -23,10 +19,12 @@ description = "~"
 bot = commands.Bot(command_prefix='+', description=description)
 REACTIONS_COMMANDS_TIMEOUT = 60.0
 
+pool = Pool()
+
 
 @bot.event
 async def on_ready():
-    logging.info("{} is now active.".format(bot.user))
+    logging.info(f'{bot.user} is active.')
 
 
 ########################################################################################################################
@@ -35,47 +33,48 @@ async def on_ready():
 
 
 async def send_card(ctx, card, evo=False):
-    logging.info("Trying to send card [{}] {} requested by {}...".format(card, "(evolved)" * evo, ctx.message.author))
-    card = pool[card]
-    embed = discord.Embed(title=card.name + " [Evolved]" * evo)
-    embed.add_field(name="Rarity:", value=card.rarity, inline=True)
-    embed.add_field(name="Craft:", value=card.craft)
-    embed.add_field(name="Type:", value=card.type, inline=True)
-    embed.add_field(name="Trait:", value=card.trait)
-    embed.add_field(name="Expansion:", value=card.expac)
-    if card.type == "Follower":
-        embed.add_field(name="Stats: ", value="{}/{} → {}/{}\n".format(card.attack, card.defense,
-                                                                       card.evo_attack, card.evo_defense), inline=False)
-        embed.add_field(name="Base effect: ", value="{}\n".format(card.effect), inline=False)
-        embed.add_field(name="Evo effect: ", value=card.evo_effect, inline=False)
+    logging.info(f'Trying to send card {card + " Evolved" if evo else ""} requested by {ctx.message.author} ')
+    card = pool.p[card]
+    embed = discord.Embed(title=card["name_"] + " Evolved" * evo)
+    embed.add_field(name="Rarity:", value=card["rarity_"], inline=True)
+    embed.add_field(name="Craft:", value=card["craft_"])
+    embed.add_field(name="Type:", value=card["type_"])
+    embed.add_field(name="Trait:", value=card["trait_"])
+    embed.add_field(name="Expansion:", value=card["expansion_"])
+    if card["type_"] == "Follower":
+        embed.add_field(name="Stats: ", value=f'{card["baseAtk_"]}/{card["baseDef_"]} → '
+                                              f'{card["evoAtk_"]}/{card["evoDef_"]}\n', inline=False)
+        embed.add_field(name="Base effect: ", value=f'{card["baseEffect_"]}\n', inline=False)
+        embed.add_field(name="Evo effect: ", value=card["evoEffect_"], inline=False)
     else:
-        embed.add_field(name="Effect:", value=card.effect)
+        embed.add_field(name="Effect:", value=card["baseEffect"])
     if evo:
-        embed.set_image(url=card.evo_pic)
-        embed.set_footer(text=card.evo_flair)
+        embed.set_image(url=pool.pic(card["name_"], True))
+        embed.set_footer(text=card["evoFlair_"])
     else:
-        embed.set_image(url=card.pic)
-        embed.set_footer(text=card.flair)
-    logging.info("about to send")
+        embed.set_image(url=pool.pic(card["name_"], False))
+        embed.set_footer(text=card["baseFlair_"])
+    logging.info("About to send...")
     msg = await ctx.send(embed=embed)
-    logging.info("...successfully sent, producing message {}.".format(msg.id))
-    if card.type == "Follower" and not evo:
+    logging.info(f'...successfully sent, producing message {msg.id}.')
+    if card["type_"] == "Follower" and not evo:
         await msg.add_reaction("🇪")  # regional indicator E
-        try:
-            await bot.wait_for("reaction_add",
-                               check=lambda r, u: str(r.emoji) == "🇪" and u != msg.author and r.message.id == msg.id,
-                               timeout=REACTIONS_COMMANDS_TIMEOUT)
-            await msg.delete()  # TODO: replace this by a message edit+ a toggle to display the normal image back
-            await send_card(ctx, card.name, evo=True)
-        except asyncio.TimeoutError:
-            logging.info("Reactable status expired on message {}.".format(msg.id))
+    try:
+        await bot.wait_for("reaction_add",
+                           check=lambda r, u: str(r.emoji) == "🇪" and u != msg.author and r.message.id == msg.id,
+                           timeout=REACTIONS_COMMANDS_TIMEOUT)
+        # TODO: replace this by a message edit+ a toggle to display the normal image back
+        await msg.delete()
+        await send_card(ctx, card["name_"], evo=True)
+    except asyncio.TimeoutError:
+        logging.info(f'Reactable status expired on message {msg.id}.')
 
 
 @bot.command(aliases=['r'], description="""
-    Displays a random card. Token cards are included by default.
+    Displays a random card[" Token cards are included by default.
     """)  # TODO: flag for excluding tokens, flag for filtering by expansion (or really, by any card attribute).
 async def random(ctx):
-    logging.info("Random card requested by {}.".format(ctx.message.author))
+    logging.info(f'Random card requested by {ctx.message.author}')
     randomCard = pool.get_random_card()
     await send_card(ctx, randomCard)
 
@@ -91,47 +90,50 @@ async def random(ctx):
         {0}abomination
         {0}blood gold summon bat
     """.format(bot.command_prefix))
-async def find(ctx, *searchTerms: list, maxMatches=15):
-    int_to_emote = {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
-                    4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣",
-                    8: "8️⃣", 9: "9️⃣", 10: "🇦", 11: "🇧",
-                    12: "🇨", 13: "🇩", 14: "🇪", 15: "🇫"}
-    emote_to_int = {i: j for j, i in int_to_emote.items()}
-
-    logging.info("Trying a card search with terms {} requested by {}...".format(searchTerms, ctx.message.author))
-    if len(searchTerms) == 1:
-        logging.info("Only one parameter passed, trying a search by name...")
-        matches = pool.search_by_name(searchTerms[0], max_matches=maxMatches)
-        if type(matches) == list and not matches:
-            logging.info("...search by name failed, trying a search by single attribute...")
-            matches = pool.search_by_attributes(searchTerms[0], max_matches=maxMatches)
-    else:
-        logging.info("Multiple parameters passed, trying a search by multiple attributes...")
-        matches = pool.search_by_attributes(*searchTerms, max_matches=maxMatches)
-    if type(matches) == int:
-        await ctx.send("{} matches found. Be more precise.".format(matches))
-    elif matches:
-        if len(matches) == 1:
-            logging.info("...exactly one card found. Sending that.")
-            await send_card(ctx, matches[0])
+async def find(ctx, search_terms: list, max_matches=15):
+    logging.info("Trying a card search with terms {} requested by {}...".format(search_terms, ctx.message.author))
+    logging.info("Trying a search by name...")
+    try:
+        if len(search_terms.split(' ')) == 1:
+            matches = pool.search_by_name(search_terms, max_matches=max_matches)
         else:
-            # [1] Send list of n result, react to it with n emotes and send an item among those, if requested.
-            embed = discord.Embed(title="Possible matches:")
-            for i in range(len(matches)):
-                embed.add_field(name=hex(i).replace('0x', '').upper(), value=matches[i])
-            msg = await ctx.send(embed=embed)
-            logging.info("...successfully found multiple matches, producing message {}.".format(msg.id))
-            for i in range(len(matches)):
-                await msg.add_reaction(int_to_emote[i])
-            try:
-                reaction, user = await bot.wait_for("reaction_add",
-                                                    check=lambda r, u: str(r.emoji) in list(emote_to_int)[:len(matches)] \
-                                                                       and u != msg.author and r.message.id == msg.id,
-                                                    timeout=REACTIONS_COMMANDS_TIMEOUT)
-                card = matches[emote_to_int[reaction.emoji]]
-                await send_card(ctx, card)
-            except asyncio.TimeoutError:
-                logging.info("Reactable status expired on message {}.".format(msg.id))
+            matches = pool.search_by_attributes(search_terms, max_matches=max_matches)
+        if len(matches) == 0:
+            if len(search_terms.split(' ')) != 1:
+                matches = pool.search_by_name(search_terms, max_matches=max_matches)
+            else:
+                matches = pool.search_by_attributes(search_terms, max_matches=max_matches)
+        if len(matches) == 0:
+            raise TooManyMatches(0)
+    except TooManyMatches as t:
+        await ctx.send(embed=discord.Embed(title=f'{t.matches} matches found. Be more precise!'))
+    if len(matches) == 1:
+        logging.info("...exactly one card found. Sending that.")
+        await send_card(ctx, matches[0])
+    elif len(matches) > 1:
+        # Send list of n result, react to it with n emotes and send an item among those, if requested.
+        embed = discord.Embed(title="Possible matches:")
+        for i in range(len(matches)):
+            embed.add_field(name=hex(i).replace('0x', '').upper(), value=matches[i])
+        msg = await ctx.send(embed=embed)
+        logging.info("...successfully found multiple matches, producing message {}.".format(msg.id))
+        int_to_emote = {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
+                        4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣",
+                        8: "8️⃣", 9: "9️⃣", 10: "🇦", 11: "🇧",
+                        12: "🇨", 13: "🇩", 14: "🇪", 15: "🇫"}
+        emote_to_int = {i: j for j, i in int_to_emote.items()}
+        for i in range(len(matches)):
+            await msg.add_reaction(int_to_emote[i])
+        try:
+            reaction, user = await bot.wait_for("reaction_add",
+                                                check=lambda r, u: str(r.emoji) in list(emote_to_int)[:len(matches)]
+                                                                   and u != msg.author and r.message.id == msg.id,
+                                                timeout=REACTIONS_COMMANDS_TIMEOUT)
+            card = matches[emote_to_int[reaction.emoji]]
+            await send_card(ctx, card)
+        except asyncio.TimeoutError:
+            logging.info("Reactable status expired on message {}.".format(msg.id))
+
 
 
 @bot.event
@@ -140,36 +142,8 @@ async def on_command_error(ctx, error):
     If the bot detects a prefix+string message, where string isn't a command, it uses the string to fire a card search.
     """
     if isinstance(error, commands.CommandNotFound):
-        searchTerms = tuple(ctx.message.content.replace(bot.command_prefix, '').split())
-        await find(ctx, *searchTerms)
-
-
-########################################################################################################################
-# CUBE COMMANDS ########################################################################################################
-########################################################################################################################
-
-# TODO
-
-########################################################################################################################
-# MEME COMMANDS ########################################################################################################
-########################################################################################################################
-
-@bot.command()
-async def trash(ctx):
-    """
-    Mattia special guest in the house tonight
-    """
-    msg = await ctx.send("🗑️")
-    await msg.add_reaction("🗑️")
-
-
-@bot.command()
-async def grid(ctx, size=12):
-    imageSet = [pool.get_random_card() for i in range(size)]
-    file = grid_merge(*imageSet)
-    file = discord.File(file)
-    await ctx.send(file=file)
-    os.remove(file)
+        search_terms = ctx.message.content.replace(bot.command_prefix, '')
+        await find(ctx, search_terms)
 
 
 bot.run(token)
